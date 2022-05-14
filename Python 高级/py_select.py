52d067580cdbd329737bfe2407ce22d102daab0f
Scott Zhang <wittyfans@outlook.com>
2022-05-14 12:05:11 +0800

    fix py select error

--- Python 高级/py_select.py
+++ Python 高级/py_select.py
@@ -21,33 +21,22 @@ class Fetcher:
         selector.unregister(key.fd)
         # 使用了事件监听，所以无需 try catch
         self.client.send(
-            "GET {} HTTP/1.1\r\nHOST:{}\r\nConnection:close\r\n\r\n".format(
-                self.path, self.host
-            ).encode("utf8")
+            "GET {} HTTP/1.1\r\nHost:{}\r\nConnection:close\r\n\r\n"
+            .format(self.path, self.host).encode("utf8")
         )
         selector.register(self.client.fileno(), EVENT_READ, self.readable)
 
 
     def readable(self, key):
-        """只要可读，就会调用 readable"""
-        # data = b""
-        # while True:
-        #     d = self.client.recv(1024)
-        #     if d:
-        #         data += d
-        #     else:
-        #         break
         d = self.client.recv(1024)
         if d:
             self.data += d
         else:
             selector.unregister(key.fd)
-
             data = self.data.decode("utf8")
-            html_data = data.split('\r\n\r\n')[1]
+            html_data = data.split("\r\n\r\n")[1]
             print(html_data)
             self.client.close()
-
             urls.remove(self.spider_url)
             if not urls:
                 global stop
@@ -57,46 +46,42 @@ class Fetcher:
     def get_url(self, url):
         self.spider_url = url
         url = urlparse(url)
-
         self.host = url.netloc
-        self.path = url .path
+        self.path = url.path
         self.data = b""
-
         if self.path == "":
             self.path = "/"
 
-        # socket connect
+        # 建立socket连接
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.setblocking(False)
 
         try:
-            self.client.connect((self.host, 80))
+            self.client.connect((self.host, 80))  # 阻塞不会消耗cpu
         except BlockingIOError as e:
             pass
 
         # 将 socket 注册到 select 中
+        # 参数是 client.fileno, 事件，回调函数，即我们需要执行的函数
         selector.register(
-            self.client.fileno(), # socket 的文件描述符
-            EVENT_WRITE,         # 事件
-            self.connected       # 回调函数，即我们需要执行的函数
+            self.client.fileno(),
+            EVENT_WRITE,
+            self.connected  
         )
 
 
-    def loop(self):
-        # event main loop, 不停的请求 socket 状态，并调用对应的回调函数
-        # 注意：
-        # select 本身不支持 register 模式
-        # socket 状态变化以后的回调由我们自己完成
-        # 返回的
-        while not stop:
-            ready = selector.select()
-            while True:
-                for key, mask in ready:
-                    callback = key.data
-                    callback(key)
+def loop():
+    #事件循环，不停的请求socket的状态并调用对应的回调函数
+    #1. select本身是不支持register模式
+    #2. socket状态变化以后的回调是由程序员完成的
+    while not stop:
+        ready = selector.select()
+        for key, mask in ready:
+            call_back = key.data
+            call_back(key)
 
 
 if __name__ == '__main__':
     fetcher = Fetcher()
     fetcher.get_url("www.baidu.com")
-    fetcher.loop()
+    loop()
